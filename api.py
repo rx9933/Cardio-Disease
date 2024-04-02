14a34a04f66e32b8f7bcce8bdc1f472223ed29ba
Ubuntu <ubuntu@alanagaughan2048-coe332-vm.js2local>
2024-04-02 23:56:41 +0000

    added method for post get delete to redis db

--- api.py
+++ api.py
@@ -5,10 +5,6 @@ from jobs import add_job, get_job_by_id, get_all_job_ids, rd, delete_jobs
 
 app = Flask(__name__)
 
-#def get_redis_client():
-#    #Change thsi line to 'redis-db' when using dockerhub
-#    return redis.Redis(host='redis-db', port=6379, db=0)
-
 @app.route('/jobs', methods=['GET'])
 def all_jobs():
     all_job_ids = get_all_job_ids()
@@ -32,34 +28,33 @@ def delete_all_jobs():
     delete_jobs()
     return "all jobs have been deleted off of worker queue. \n"
 
-#@app.route('/data', methods= ['GET', 'POST', 'DELETE'])
-#def edit_redis_data():
-#    '''
-#    Edits the redis database. 
-#    If method = POST, Posts the data into the redis data base
-#    If method = GET, Returns all data in the db, 
-#        Outputs: return_list (list), list of dictionaries containing all data in the db
-#    if method = DELETE, Deletes all data from db
-#    '''
-#    rd = get_redis_client()
-#    if request.method == 'POST':
-#        response = requests.get(url="https://data.cdc.gov/resource/ikwk-8git.json")
-#        data = response.json()
-#        for row in data:
-#            # Adding the data to redis as a hash with teh key being teh row id
-#            row_id = row['row_id']
-#            rd.hset(row_id, mapping=row)
-#        return "Data posted successfully"
-#        return row
-#    if request.method == 'GET':
-#        return_list = []
-#        for key in rd.keys():
-#            key_data = rd.hgetall(key)
-#            return_list.append(key_data)
-#        return return_list
-#    if request.method == 'DELETE':
-#        rd.flushdb()
-#        return "Data deleted successfully"
+@app.route('/data', methods= ['GET', 'POST', 'DELETE'])
+def edit_redis_data():
+    '''
+    Edits the redis database. 
+    If method = POST, Posts the data into the redis data base
+    If method = GET, Returns all data in the db, 
+        Outputs: return_list (list), list of dictionaries containing all data in the db
+    if method = DELETE, Deletes all data from db
+    '''
+    rd = get_redis_client()
+    if request.method == 'POST':        
+        response = requests.get(url="https://data.cdc.gov/resource/ikwk-8git.json")
+        data = response.json()
+        for row in data:
+            # Adding the data to redis as a hash with teh key being teh row id
+            row_id = str(row['row_id'])
+            rd.hset(row_id, mapping=row)
+        return "Data posted successfully"
+    if request.method == 'GET':
+        return_list = []
+        for key in rd.keys():
+            key_data = rd.hgetall(key)
+            return_list.append(key_data)
+        return return_list
+    if request.method == 'DELETE':
+        rd.flushdb()
+        return "Data deleted successfully"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
